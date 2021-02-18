dfc7ec1ca5d9920075323b909dafb7d8818d418f
Maarten de Jong <maarten.de.jong@hotmail.com>
2021-02-18 14:13:12 +0100

    Create preloader for full_process simplify

--- lasSplitter.py
+++ lasSplitter.py
@@ -1,10 +1,8 @@
 import math
 import os
 import subprocess
-import time
 
 from laspy.file import File
-from numpy.ma import masked_array
 
 from thirdparty.pointio import io_las, io_npy
 
@@ -23,10 +21,11 @@ class Bbox:
         return self.max_y - self.min_y
 
 
-cell_size = 50
+cell_size = 100
 simplify_epsilon = 0.2
 
-input_file = "H:\\LAZ\\c_37en1_big_delft.laz"
+input_file = "data\\crop.laz"
+# input_file = "F:\\LAZ\\C_37EN1.LAZ"
 output_directory = "sliced"
 npy_directory = "npyd"
 
@@ -49,28 +48,45 @@ cell_count = pow(2, math.ceil(math.log(max_dimension, 2)))
 print("{0}x{1}, {2}".format(cell_count, cell_count, cell_size))
 
 # Split to tiles of cell_size x cell_size with lastile
-subprocess.run(["thirdparty\\lastools\\lastile", "-i", input_file, "-tile_size", str(cell_size), "-o", output_directory + "\\" + input_file.split("\\")[1]])
+# subprocess.run(["thirdparty\\lastools\\lastile", "-i", input_file, "-tile_size", str(cell_size), "-o", output_directory + "\\" + input_file.split("\\")[1]])
+#
+# for file in os.listdir(output_directory):
+#     print(output_directory + "\\" + file)
+#
+#     datadict = io_las.read_las(output_directory + "\\" + file)
+#     io_npy.write_npy(npy_directory + "\\" + file.split(".")[0], datadict, ['coords', 'offset'])
 
-for file in os.listdir(output_directory):
-    print(output_directory + "\\" + file)
-
-    datadict = io_las.read_las(output_directory + "\\" + file)
-    io_npy.write_npy(npy_directory + "\\" + file.split(".")[0], datadict, ['coords', 'offset'])
-
-    time.sleep(1)
+datadict = io_las.read_las(input_file)
+io_npy.write_npy(npy_directory + "\\37en1", datadict, ['coords', 'offset'])
 
 
 for subdirectory in os.listdir(npy_directory):
-    subprocess.run(["thirdparty\\masbcpp\\compute_normals", npy_directory + "\\" + subdirectory])
-    subprocess.run(["thirdparty\\masbcpp\\compute_ma", npy_directory + "\\" + subdirectory])
-    subprocess.run(["thirdparty\\masbcpp\\simplify", "-i", "-e", str(simplify_epsilon), npy_directory + "\\" + subdirectory])
+
+    compute_all = subprocess.Popen(["thirdparty\\masbcpp\\full_process", npy_directory + "\\" + subdirectory])
+    compute_all.communicate()
+
+    # compute_normals = subprocess.Popen(["thirdparty\\masbcpp\\compute_normals", npy_directory + "\\" + subdirectory])
+    # compute_normals.communicate()
+    #
+    # compute_ma = subprocess.Popen(["thirdparty\\masbcpp\\compute_ma", npy_directory + "\\" + subdirectory])
+    # compute_ma.communicate()
+    #
+    # simplify = subprocess.Popen(["thirdparty\\masbcpp\\simplify", "-i", "-e", str(simplify_epsilon), npy_directory + "\\" + subdirectory])
+    # simplify.communicate()
+
+    removed_decimate = False
+    if os.path.getsize(npy_directory + "\\" + subdirectory + "\\" + "decimate_lfs.npy") == 0:
+        os.remove(npy_directory + "\\" + subdirectory + "\\" + "decimate_lfs.npy")
+        removed_decimate = True
 
     datadict = io_npy.read_npy(npy_directory + "\\" + subdirectory)
-    io_las.write_las("simplified" + "\\" + subdirectory + ".laz", datadict, datadict['decimate_lfs'])
+
+    if not removed_decimate:
+        io_las.write_las("simplified" + "\\" + subdirectory + ".laz", datadict, datadict['decimate_lfs'])
+    else:
+        io_las.write_las("simplified" + "\\" + subdirectory + ".laz", datadict)
 
     outfile = open("simplified" + "\\" + subdirectory + ".obj", mode="w")
     sstfin = subprocess.Popen(["thirdparty\\sst\\sstfin.exe", "simplified" + "\\" + subdirectory + ".laz", "10"], stdout=subprocess.PIPE)
     output = subprocess.Popen(["thirdparty\\sst\\sstdt.exe"], stdin=sstfin.stdout, stdout=outfile)
-
-    time.sleep(1)
 
