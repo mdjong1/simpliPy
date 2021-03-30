00281d108a0577ee92e12500cd7a0b8698bca4dd
Maarten de Jong <maarten.de.jong@hotmail.com>
2021-03-30 14:00:26 +0200

    Run finalization in separate processes
    This allows insertion to continue in background and multiple cells to be finalized in parallel (depending on available resources). Each finalization currently pulls 100% of a core, and ~1GB memory

--- direct_refinement.py
+++ direct_refinement.py
@@ -1,10 +1,12 @@
 import sys
+import time
+
 import startin
 
 import numpy as np
 
 from math import floor
-
+from multiprocessing import cpu_count, Process
 
 COARSE_THRESHOLD = 1
 FINE_THRESHOLD = 0.2
@@ -22,14 +24,12 @@ class Triangulation:
 
         self.grid_dimensions = None
         self.grid_points = None
-        self.triangulations = None
+        self.initial_points = None
 
         self.min_x = None
         self.min_y = None
         self.max_x = None
         self.max_y = None
-
-        self.vertices = {}
 
     def set_bbox(self, min_x, min_y, max_x, max_y):
         self.min_x = min_x
@@ -37,12 +37,58 @@ class Triangulation:
         self.max_x = max_x
         self.max_y = max_y
 
-        self.initialize_triangulations()
+    def initialize_grid(self, grid_size):
+        self.grid_dimensions = grid_size
+        self.grid_points = np.empty(shape=(grid_size, grid_size), dtype=object)
+        self.initial_points = np.empty(shape=(grid_size, grid_size), dtype=object)
 
-    def initialize_triangulations(self):
-        for x in np.arange(self.min_x, self.max_x, self.cell_size):
-            for y in np.arange(self.min_y, self.max_y, self.cell_size):
-                grid_cell = self.get_cell(x, y)
+    def insert_point(self, x, y, z, grid_cell, write_vertex):
+        if type(self.grid_points[grid_cell[0]][grid_cell[1]]) == list:
+            self.grid_points[grid_cell[0]][grid_cell[1]].append([x, y, z])
+        else:
+            self.grid_points[grid_cell[0]][grid_cell[1]] = [[x, y, z]]
+
+        if write_vertex:
+            if type(self.initial_points[grid_cell[0]][grid_cell[1]]) == list:
+                self.initial_points[grid_cell[0]][grid_cell[1]].append([x, y, z])
+            else:
+                self.initial_points[grid_cell[0]][grid_cell[1]] = [[x, y, z]]
+
+
+
+    def insert_point_in_grid(self, x, y, z):
+        grid_cell = self.get_cell(x, y)
+
+        write_vertex = False
+
+        # Always include points on outer bbox
+        if is_almost(x, self.min_x + grid_cell[0] * self.cell_size, abs_tol=0.001) or \
+                is_almost(x, self.min_x + grid_cell[0] * self.cell_size + self.cell_size, abs_tol=0.001) or \
+                is_almost(y, self.min_y + grid_cell[1] * self.cell_size, abs_tol=0.001) or \
+                is_almost(y, self.max_y + grid_cell[1] * self.cell_size + self.cell_size, abs_tol=0.001):
+
+            write_vertex = True
+
+        self.insert_point(x, y, z, grid_cell, write_vertex)
+
+    def get_cell(self, x, y):
+        return floor((x - self.min_x) / self.cell_size), floor((y - self.min_y) / self.cell_size)
+
+    def finalize(self, grid_x, grid_y, input_line):
+
+        if self.grid_points[grid_x][grid_y] is not None:
+
+            triangulation = startin.DT()
+
+            # if self.initial_points[grid_x][grid_y] is not None:
+            #     triangulation.insert(self.initial_points[grid_x][grid_y])
+            #
+            #     self.initial_points[grid_x][grid_y] = None
+
+            # Apparently not enough border points so creating artificial ones..
+            if triangulation.number_of_vertices() < 4:
+                x = self.min_x + grid_x * self.cell_size
+                y = self.min_y + grid_y * self.cell_size
 
                 initial_points = [
                     [x, y, 0],
@@ -51,76 +97,38 @@ class Triangulation:
                     [x + self.cell_size, y + self.cell_size, 0]
                 ]
 
-                dt = startin.DT()
-                dt.insert(initial_points)
-
-                self.triangulations[grid_cell[0]][grid_cell[1]] = dt
-
-    def initialize_grid(self, grid_size):
-        self.grid_dimensions = grid_size
-        self.grid_points = np.empty(shape=(grid_size, grid_size), dtype=object)
-        self.triangulations = np.empty(shape=(grid_size, grid_size), dtype=object)
-
-    def insert_point(self, x, y, z, grid_cell, write_vertex):
-        if type(self.grid_points[grid_cell[0]][grid_cell[1]]) == list:
-            self.grid_points[grid_cell[0]][grid_cell[1]].append([write_vertex, x, y, z])
-        else:
-            self.grid_points[grid_cell[0]][grid_cell[1]] = [[write_vertex, x, y, z]]
-
-        if write_vertex:
-            self.triangulations[grid_cell[0]][grid_cell[1]].insert_one_pt(x, y, z, 0)
-
-    def insert_point_in_grid(self, x, y, z):
-        grid_cell = self.get_cell(x, y)
-
-        write_vertex = False
-
-        # Always include points on outer bbox
-        if is_almost(x, self.min_x, abs_tol=0.0001) or is_almost(x, self.max_x, abs_tol=0.001) or \
-                is_almost(y, self.min_y, abs_tol=0.001) or is_almost(y, self.max_y, abs_tol=0.001):
-
-            write_vertex = True
-
-        else:
-            interpolated_value = self.triangulations[grid_cell[0]][grid_cell[1]].interpolate_tin_linear(x, y)
-
-            if abs(interpolated_value - z) > COARSE_THRESHOLD:
-                # sys.stderr.write("1: {} - {} = {}\n".format(interpolated_value, z, abs(interpolated_value - z)))
-
-                write_vertex = True
-
-        self.insert_point(x, y, z, grid_cell, write_vertex)
-
-    def get_cell(self, x, y):
-        return floor((x - self.min_x) / self.cell_size), floor((y - self.min_y) / self.cell_size)
-
-    def finalize(self, grid_x, grid_y):
-        if self.grid_points[grid_x][grid_y] is not None:
+                triangulation.insert(initial_points)
 
             for vertex in self.grid_points[grid_x][grid_y]:
-                if vertex[0] is False:
+                    x = vertex[0]
+                    y = vertex[1]
+                    z = vertex[2]
 
-                    x = vertex[1]
-                    y = vertex[2]
-                    z = vertex[3]
+                    try:
+                        interpolated_value = triangulation.interpolate_tin_linear(x, y)
 
-                    interpolated_value = self.triangulations[grid_x][grid_y].interpolate_tin_linear(x, y)
+                        if abs(interpolated_value - z) > COARSE_THRESHOLD:
+                            triangulation.insert_one_pt(x, y, z, 0)
 
-                    if abs(interpolated_value - z) > FINE_THRESHOLD:
-                        # sys.stderr.write("2: {} - {} = {}\n".format(interpolated_value, z, abs(interpolated_value - z)))
-                        self.insert_point(x, y, z, [grid_x, grid_y], write_vertex=True)
+                    except OSError:
+                        pass
 
-            for vertex in self.grid_points[grid_x][grid_y]:
-                if vertex[0] is True:
-                    sys.stdout.write("v " + str(vertex[1]) + " " + str(vertex[2]) + " " + str(vertex[3]) + "\n")
+            for vertex in triangulation.all_vertices():
+                # Don't print infinite vertex
+                if vertex[0] > 0:
+                    sys.stdout.write("v " + str(vertex[0]) + " " + str(vertex[1]) + " " + str(vertex[2]) + "\n")
 
-        self.triangulations[grid_x][grid_y] = None
-        self.grid_points[grid_x][grid_y] = None
+            self.grid_points[grid_x][grid_y] = None
+
+        sys.stdout.write(input_line)
+        sys.stdout.flush()
 
 
 class Processor:
     def __init__(self, dt):
-        self._triangulation = dt
+        self.triangulation = dt
+
+        self.processes = []
 
     def process_line(self, input_line):
         split_line = input_line.rstrip("\n").split(" ")
@@ -133,32 +141,49 @@ class Processor:
 
         elif identifier == "n":
             # Total number of points
-            self._triangulation.total_points = int(data[0])
+            self.triangulation.total_points = int(data[0])
             sys.stdout.write(input_line)
 
         elif identifier == "c":
             # Grid dimensions (cXc)
-            self._triangulation.initialize_grid(int(data[0]))
+            self.triangulation.initialize_grid(int(data[0]))
             sys.stdout.write(input_line)
 
         elif identifier == "s":
             # Cell size
-            self._triangulation.cell_size = int(data[0])
+            self.triangulation.cell_size = int(data[0])
             sys.stdout.write(input_line)
 
         elif identifier == "b":
             # bbox
-            self._triangulation.set_bbox(float(data[0]), float(data[1]), float(data[2]), float(data[3]))
+            self.triangulation.set_bbox(float(data[0]), float(data[1]), float(data[2]), float(data[3]))
             sys.stdout.write(input_line)
 
         elif identifier == "v":
             # vertex
-            self._triangulation.insert_point_in_grid(float(data[0]), float(data[1]), float(data[2]))
+            self.triangulation.insert_point_in_grid(float(data[0]), float(data[1]), float(data[2]))
 
         elif identifier == "x":
             # cell finalizer
-            self._triangulation.finalize(int(data[0]), int(data[1]))
-            sys.stdout.write(input_line)
+
+            sys.stderr.write("Starting new process to finalize: {}. Processing currently running: {}\n".format(data, len(self.processes)))
+            sys.stderr.flush()
+
+            sleep_time = 0.01
+
+            # Ensure total number of processes never exceeds capacity
+            while len(self.processes) >= cpu_count() - 4:
+                for i in reversed(range(len(self.processes))):
+                    if not self.processes[i].is_alive():
+                        del self.processes[i]
+
+                # Incremental sleep timer
+                time.sleep(sleep_time)
+                sleep_time *= 1.5
+
+            process = Process(target=self.triangulation.finalize, args=(int(data[0]), int(data[1]), input_line,))
+            self.processes.append(process)
+            process.start()
 
         else:
             # Unknown identifier in stream
@@ -171,5 +196,8 @@ if __name__ == "__main__":
     triangulation = Triangulation()
     processor = Processor(triangulation)
 
-    for stdin_line in sys.stdin.readlines():
+    for stdin_line in sys.stdin:
         processor.process_line(stdin_line)
+
+    for process in processor.processes:
+        process.join()
