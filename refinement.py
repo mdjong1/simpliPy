e5da4b01236de109ee300e6a4d8cebd70de11bf8
Maarten de Jong <maarten.de.jong@hotmail.com>
2021-04-12 14:41:50 +0200

    Version of refinement using loop to find max value

--- refinement.py
+++ refinement.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import time
 
@@ -5,18 +6,27 @@ import startin
 
 import numpy as np
 
-from heapq import heappop, heapify, _siftup, _siftdown
+from heapq import heappop, heapify
 from multiprocessing import cpu_count, Process
 from scipy.spatial import KDTree
 
 TRIANGULATION_THRESHOLD = 0.2
+DELTA_PRECISION = 1E4
 
+
+class Vertex:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.delta_z = 0
+
+    def __str__(self):
+        return "{} {} {} - {}\n".format(self.x, self.y, self.z, self.delta_z)
 
 class Triangulation:
     def __init__(self):
-        self.total_points = None
         self.cell_size = None
-        self.grid_dimensions = None
 
         self.min_x = None
         self.min_y = None
@@ -33,22 +43,21 @@ class Triangulation:
         self.max_y = max_y
 
     def insert_vertex(self, x, y, z):
-        self.vertices[self.vertex_id] = [x, y, z]
+        self.vertices[self.vertex_id] = Vertex(x, y, z)
         self.vertex_id += 1
 
     def finalize(self, input_line, grid_x, grid_y, vertices):
         if len(vertices) > 0:
             triangulation = startin.DT()
 
-            heap = []
             x_vals = []
             y_vals = []
             z_vals = []
 
             for vertex_id, vertex in vertices.items():
-                x_vals.append(vertex[0])
-                y_vals.append(vertex[1])
-                z_vals.append(vertex[2])
+                x_vals.append(vertex.x)
+                y_vals.append(vertex.y)
+                z_vals.append(vertex.z)
 
             tree = KDTree(np.c_[x_vals, y_vals])
 
@@ -72,52 +81,57 @@ class Triangulation:
 
             triangulation.insert(near_corner_points)
 
+            heap = []
+            worst_point_index = 1
+
             for vertex_id, vertex in vertices.items():
-
                 try:
-                    interpolated_value = triangulation.interpolate_tin_linear(vertex[0], vertex[1])
+                    interpolated_value = triangulation.interpolate_tin_linear(vertex.x, vertex.y)
 
-                    # Heap is min-based, so multiply by -1 to ensure max delta is at top
-                    delta = abs(interpolated_value - vertex[2]) * -1
+                    vertex.delta_z = round(abs(interpolated_value - vertex.z) * DELTA_PRECISION) / DELTA_PRECISION
 
-                    heap.append((delta, vertex))
+                    if vertex.delta_z > vertices[worst_point_index].delta_z:
+                        worst_point_index = vertex_id
 
+                # If outside CH, always insert
                 except OSError:
-                    
                     triangulation.insert_one_pt(vertex[0], vertex[1], vertex[2], 0)
 
-            heapify(heap)
+            # heapify(heap)
+
+            loop_time = time.time()
+
+            largest_delta = vertices[worst_point_index]
 
             while True:
-                try:
-                    largest_delta = heappop(heap)
-                except IndexError:
-                    break
+                if len(vertices) % 100 == 0:
+                    sys.stderr.write("Vertices left: {}, time since last 100: {}\n".format(len(vertices), time.time() - loop_time))
+                    sys.stderr.flush()
+                    loop_time = time.time()
 
                 try:
-                    if largest_delta[0] * -1 > TRIANGULATION_THRESHOLD:
-                        triangulation.insert_one_pt(largest_delta[1][0], largest_delta[1][1], largest_delta[1][2], 0)
+                    if largest_delta.delta_z > TRIANGULATION_THRESHOLD:
+                        triangulation.insert_one_pt(largest_delta.x, largest_delta.y, largest_delta.z, 0)
                     else:
                         break
 
+                # Somehow point is outside bbox, ignore
                 except OSError:
                     pass
-                    # sys.stdout.write("Managed to find one outside CH\n")
-                    # triangulation.insert_one_pt(largest_delta[1][0], largest_delta[1][1], largest_delta[1][2], 0)
 
-                # For every 10 points; recalculate the entire heap's errors
-                if len(heap) % 10 == 0:
-                    sys.stderr.write("Size of heap: {}\n".format(len(heap)))
-                    for i in range(len(heap)):
-                        new_val = abs(triangulation.interpolate_tin_linear(heap[i][1][0], heap[i][1][1]) - heap[i][1][2]) * -1
+                for vertex_id, vertex in vertices.items():
+                    interpolated_value = triangulation.interpolate_tin_linear(vertex.x, vertex.y)
 
-                        old_val = heap[i]
-                        heap[i] = (new_val, heap[i][1])
+                    # Heap is min-based, so multiply by -1 to ensure max delta is at top
+                    vertex.delta_z = round(abs(interpolated_value - vertex.z) * DELTA_PRECISION) / DELTA_PRECISION
 
-                        if new_val > old_val[0]:
-                            _siftup(heap, i)
-                        else:
-                            _siftdown(heap, 0, i)
+                    if vertex.delta_z > vertices[worst_point_index].delta_z:
+                        worst_point_index = vertex_id
+
+                largest_delta = vertices[worst_point_index]
+
+                del vertices[worst_point_index]
+                worst_point_index = random.choice(list(vertices.keys()))
 
             for vertex in triangulation.all_vertices():
                 if vertex[0] > 0:  # Exclude infinite vertex
@@ -166,33 +180,27 @@ class Processor:
 
         elif identifier == "v":
             # vertex
-            self.triangulation.insert_vertex(float(data[0]), float(data[1]), float(data[2]))
+            # All sprinkle points get passed to output directly
+            if not self.sprinkling:
+                self.triangulation.insert_vertex(float(data[0]), float(data[1]), float(data[2]))
+
+            else:
+                sys.stdout.write(input_line)
 
         elif identifier == "x":
             # cell finalizer
-            # self._triangulation.finalize(int(data[0]), int(data[1]), input_line)
-
-            # thread = threading.Thread(target=self._triangulation.finalize, args=(int(data[0]), int(data[1]), input_line,), daemon=True)
-            # self.threads.append(thread)
-            # thread.start()
-
-            # cell finalizer
-
             # While sprinkling, don't bother processing since all finalized cells now are still empty anyways
             if self.sprinkling:
                 sys.stdout.write(input_line)
                 return
 
-            sys.stderr.write(
-                "Starting new process to finalize cell: {}, {}. Processing currently running: {}\n".format(data[0],
-                                                                                                           data[1], len(
-                        self.processes)))
+            sys.stderr.write("Starting new process to finalize cell: {}, {}. Processing currently running: {}\n".format(data[0], data[1], len(self.processes)))
             sys.stderr.flush()
 
             sleep_time = 1
 
             # Ensure total number of processes never exceeds capacity
-            while len(self.processes) >= cpu_count() - 2:
+            while len(self.processes) >= cpu_count() - 4:
                 for i in reversed(range(len(self.processes))):
                     if not self.processes[i].is_alive():
                         del self.processes[i]
