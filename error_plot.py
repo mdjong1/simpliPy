dcb8276ca04f05997fb3ca49d49c8cdc4ee156df
Maarten de Jong <maarten.de.jong@hotmail.com>
2021-05-06 10:52:10 +0200

    Expand on plot of errors

--- error_plot.py
+++ error_plot.py
@@ -1,3 +1,5 @@
+import math
+
 import geojson
 
 import numpy as np
@@ -18,12 +20,30 @@ for vertex_id in range(len(gj["features"])):
 
 errors = np.array(errors)
 
-bins = np.linspace(0, 10, 44)
+print("Number of vertices tested = {}".format(len(errors)))
+
+rmse = math.sqrt(np.average(errors))
+print("RMSE = {}".format(rmse))
+
+bins = np.linspace(0, 8, 40)
 
 plt.hist(errors, bins=bins, log=True)
+
+# https://stackoverflow.com/questions/16180946/drawing-average-line-in-histogram-matplotlib
+min_ylim, max_ylim = plt.ylim()
+
+plt.axvline(0.2, color='k', linestyle='dashed', linewidth=1)
+plt.text(0.2, max_ylim * 1.2, 'Threshold: {:.2f}'.format(0.2))
+
+plt.axvline(rmse, color='r', linestyle='dashed', linewidth=1)
+plt.text(rmse * 1.2, max_ylim * 0.3, 'RMSE: {:.2f}'.format(rmse))
+
+plt.axvline(errors.mean(), color='b', linestyle='dashed', linewidth=1)
+plt.text(errors.mean() * 1.4, max_ylim * 0.6, 'Mean: {:.2f}'.format(errors.mean()))
 
 plt.xlabel("Vertex z-error (m)")
 plt.ylabel("Frequency")
 
 plt.show()
+
 
