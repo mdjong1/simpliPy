132ad11ecaa15b76834fefeb1d7ba2848ddfafd3
Maarten de Jong <maarten.de.jong@hotmail.com>
2021-05-18 12:29:37 +0200

    Add streaming counter of vertices and triangles

---
+++ streaming_vertex_counter.py
@@ -0,0 +1,25 @@
+simplified_dt = "F:\\LAZ\\twotiles_full.obj"
+
+vertex_count = 0
+face_count = 0
+
+print("Starting vertex counting")
+
+print(simplified_dt)
+
+for line in open(simplified_dt, "r"):
+    split_line = line.split(" ")
+    identifier = split_line[0]
+    data = split_line[1:]
+
+    if identifier == "v":
+        vertex_count += 1
+
+    elif identifier == "f":
+        face_count += 1
+
+
+print("Number of vertices: {}".format(vertex_count))
+
+print("Number of triangles: {}".format(face_count))
+
