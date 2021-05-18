3d54145ebef5f250f470a10f445448224b0623e0
Maarten de Jong <maarten.de.jong@hotmail.com>
2021-05-18 12:32:02 +0200

    Adjust LAZ error comparison to use less memory

--- compare_error_laz.py
+++ compare_error_laz.py
@@ -3,14 +3,17 @@ import subprocess
 
 import startin
 
+from math import floor
 from geojson import Point, Feature, FeatureCollection, dump
 
+simplified_dt = "H:\\LAZ\\twotiles_rand7_clipped.obj"
 
-simplified_dt = "H:\\LAZ\\fourtiles_clipped.obj"
+original_laz = "H:\\LAZ\\twotiles_clipped.laz"
 
-original_laz = "F:\\LAZ\\fourtiles_clipped_groundonly.laz"
+output_file = "H:\\LAZ\\errors_twotiles_rand7_clipped.json"
 
-output_file = "H:\\LAZ\\errors_fourtiles_clipped.json"
+# interpolate 1/THINNING points
+THINNING = 10
 
 
 # Load simplified DT into Startin ✔
@@ -24,7 +27,7 @@ for line in open(simplified_dt, "r"):
     data = split_line[1:]
 
     if identifier == "v":
-        simplified_triangulation.insert_one_pt(float(data[0]), float(data[1]), float(data[2]), 0)
+        simplified_triangulation.insert_one_pt(float(data[0]), float(data[1]), float(data[2]))
 
 print("Loaded up all {} of the vertices from the simplified DT".format(simplified_triangulation.number_of_vertices()))
 
@@ -33,52 +36,38 @@ original_vertices = []
 
 las2txt = subprocess.Popen(["thirdparty\\lastools\\las2txt", "-i", original_laz, "-stdout"], stdout=subprocess.PIPE)
 
-for line in las2txt.stdout:
-    stripped_line = str(line.rstrip(), "utf-8")
-    split_line = stripped_line.split()
-    split_line = [float(val) for val in split_line]
-
-    x = split_line[0]
-    y = split_line[1]
-    z = split_line[2]
-
-    original_vertices.append([float(x), float(y), float(z)])
-
 # For each vertex in the original DT ✔
     # Interpolate the error for this vertex ✔
     # Attach error for vertex to vertex class ✔
 
 # https://gis.stackexchange.com/questions/130963/write-geojson-into-a-geojson-file-with-python
+
 features = []
 
-last_percentage = -1
+for line in las2txt.stdout:
+    if random.randint(0, THINNING) == floor(THINNING / 2):
+        stripped_line = str(line.rstrip(), "utf-8")
+        split_line = stripped_line.split()
+        split_line = [float(val) for val in split_line]
 
-# interpolate 1/10 points
-THINNING = 10
+        x = split_line[0]
+        y = split_line[1]
+        z = split_line[2]
 
-print("Going to interpolate {} vertices!".format(len(original_vertices)))
-
-for i in range(len(original_vertices)):
-    vertex = original_vertices[i]
-
-    if i % 10000 == 0:
-        percentage = round((i / len(original_vertices)) / 100)
-        print(percentage, "% done")
-        if percentage != last_percentage:
-            print(percentage, "% done")
-            last_percentage = percentage
-
-    if random.randint(0, THINNING) == THINNING / 2:
         try:
-            interpolated_value = simplified_triangulation.interpolate_tin_linear(vertex[0], vertex[1])
+            interpolated_value = simplified_triangulation.interpolate_tin_linear(x, y)
 
-            features.append(Feature(geometry=Point((vertex[0], vertex[1], vertex[2])), properties={"error": vertex[2] - interpolated_value}))
+            features.append(Feature(geometry=Point((x, y, z)), properties={"error": z - interpolated_value}))
 
         except OSError:
-            print("Could not interpolate {}, {}".format(vertex[0], vertex[1]))
+            print("Could not interpolate {}, {}".format(x, y))
+
+print("Creating feature collection")
 
 # Output each vertex to a GeoJSON file including attribute error ✔
 feature_collection = FeatureCollection(features)
+
+print("Writing to file")
 
 with open(output_file, "w") as f:
     dump(feature_collection, f)
